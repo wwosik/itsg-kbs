94561a2da066366c710622c71a7d73f83f9df188
Wojciech Wosik <wojciech@wosik.pl>
2023-02-13 15:51:20 +0100

    Initial Migrations

---
+++ ITSG.KBS.Migrations/global.usings.cs
@@ -0,0 +1,10 @@
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Text.Json;
+global using System.Net.Http;
+global using System.Net.Http.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Net.Http.Headers;
