d39d357fe366404d596a9b6aec0b4911360b9387
Wojciech Wosik <wojciech@wosik.pl>
2023-02-16 08:54:47 +0100

    Base Angular app

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kbs-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'kbs';
+}
