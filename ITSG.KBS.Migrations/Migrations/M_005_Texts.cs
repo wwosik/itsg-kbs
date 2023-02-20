78efbf5b927420df8e5ca2b511adfc95644625c5
Wojtek Wosik <wojciech.wosik@pta.de>
2023-02-20 18:18:09 +0100

    Better table name

--- ITSG.KBS.Migrations/Migrations/M_005_Texts.cs
+++ ITSG.KBS.Migrations/Migrations/M_005_Texts.cs
@@ -11,9 +11,9 @@ public class M_005_Texts : Migration
 
     public override void Up()
     {
-        Create.Table("Texts").WithDescription("Texte der Anwendung")
+        Create.Table("AppText").WithDescription("Texte der Anwendung")
             .WithColumn("Key").AsString(1000, "Latin1_General_CI_AS").NotNullable().PrimaryKey().WithColumnDescription("Schlüssel")
-            .WithColumn("IsMarkdown").AsFixedLengthString(1, "Latin1_General_CI_AS").ForeignKey("FK_Texts_IsMarkdown", "Ref_YesNo", "Value").NotNullable()
+            .WithColumn("IsMarkdown").AsFixedLengthString(1, "Latin1_General_CI_AS").ForeignKey("FK_AppText_IsMarkdown", "Ref_YesNo", "Value").NotNullable()
                 .WithColumnDescription("Definiert ob der Wert Markdown ist").WithDefaultValue("N")
             .WithColumn("Description").AsString(int.MaxValue, "Latin1_General_CI_AS").NotNullable().WithColumnDescription("Hilfstext für Übersetzer")
             .WithColumn("Value").AsString(int.MaxValue, "Latin1_General_CI_AS").NotNullable().WithColumnDescription("Text");
